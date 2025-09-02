4bfd989d18939408e6181f140729ac936aa05acc
Kolozuz <110570465+Kolozuz@users.noreply.github.com>
2025-09-02 18:10:18 -0500

    feat: add neovim config

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+}
