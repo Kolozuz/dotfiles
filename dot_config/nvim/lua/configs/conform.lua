dc9f75d2e1522e1f77e7963a4ff46a7bb0862445
Kolozuz <110570465+Kolozuz@users.noreply.github.com>
2025-12-16 21:59:41 -0500

    feat: set prettier as yaml formatter and increase timeout cause this shi is slow as hell and always times out

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -1,18 +1,19 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "biome" },
-    typescript = { "biome" },
-    vue = { "biome" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "biome" },
+		typescript = { "biome" },
+		vue = { "biome" },
+		yaml = { "prettier" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 2500,
+		lsp_fallback = true,
+	},
 }
 
 return options
