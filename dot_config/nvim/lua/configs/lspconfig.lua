3eb3d8db53f531444ba7754e31cbb4800f0f3997
kolozuz <110570465+Kolozuz@users.noreply.github.com>
2026-02-28 19:24:52 -0500

    feat: add lsp config for Java development, and remove angular cause we don't do none of that here

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "vtsls", "vue_ls", "angularls", "pyright", "rust_analyzer" }
+local servers = {
+	"html",
+	"cssls",
+	"vtsls",
+	"vue_ls",
+	"pyright",
+	"rust_analyzer",
+	"phpactor",
+	"jdtls",
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
